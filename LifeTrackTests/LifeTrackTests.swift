fb16b9c9b9702fdce7e5f817a8140b05c7dd43cb
rakeshtella8 <rakeshtella8@gmail.com>
2025-07-01 10:52:38 +0530

    Initial Commit

---
+++ LifeTrackTests/LifeTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTrackTests.swift
+//  LifeTrackTests
+//
+//  Created by rakesh tella on 01/07/25.
+//
+
+import Testing
+@testable import LifeTrack
+
+struct LifeTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
